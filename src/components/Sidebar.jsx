512c81f1fe337bf32a5d98b2813408e650d0b643
sherzod-uralov <sherzoduralov432@gmail.com>
2024-01-27 23:42:13 +0500

    add

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,65 +1,58 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import {HiUsers} from 'react-icons/hi2'
-import {GoStarFill} from 'react-icons/go'
-import {PiClockCounterClockwiseBold} from 'react-icons/pi'
-import {IoMdSettings} from 'react-icons/io'
-import {SiHomeassistantcommunitystore} from 'react-icons/si'
-import {FaClipboardList} from 'react-icons/fa'
-import {GiDiscGolfBag} from 'react-icons/gi'
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { HiUsers } from 'react-icons/hi2';
+import { IoMdSettings } from 'react-icons/io';
+import { MdAnalytics, MdMyLocation } from "react-icons/md";
 import { IoMdCart } from "react-icons/io";
-import { MdAnalytics } from "react-icons/md";
-import { MdMyLocation } from "react-icons/md";
 
-
-export default function Sidebar({children}) {
-
-    const menuItem=[
+export default function Sidebar({ children }) {
+    const location = useLocation(); // Get the current location
+    const menuItem = [
         {
-            path:'/',
-            icon:<MdAnalytics />
-
-            
-          },
-          {
-            path:'/order',
-            icon:<IoMdCart/>
-          },
-    
-        {
-          path:'/login',
-          icon:<HiUsers/>
+            path: '/',
+            icon: <MdAnalytics />,
+            border:'bl'
         },
-        
         {
-            path:'',
-            icon:<IoMdSettings />
-          }, 
-          {
-            path:'',
-            icon:<MdMyLocation />
-          }, 
-          
-    
-      ]
-
-
-  return (
-    <div className='flex h-screen'>
-        <div className='h-screen border-r-[2px] border-gray-100 bg-white block'>
-        <div className='rounded-full text-center text-white py-2 m-4 bg-blue-600 font-semibold text-xl'>D</div>
+            path: '/order',
+            icon: <IoMdCart />
+        },
         {
-         menuItem.map((item,index)=>(
-         <NavLink className='text-gray-500  mt-4 text-xl' to={item.path} key={index}  >
-            
-            <div  className='text-center mx-auto px-7 py-3 font-semibold hover:text-blue-600 hover:bg-gray-200 active:bg-gray-300 focus:bg-gray-200 focus:text-blue-600'>{item.icon}</div>
-         
-        
-         </NavLink>
-         ))
-         }
+            path: '/login',
+            icon: <HiUsers />
+        },
+        {
+            path: '',
+            icon: <IoMdSettings />
+        },
+        {
+            path: '',
+            icon: <MdMyLocation />
+        },
+    ];
+
+    return (
+        <div className='flex h-screen'>
+            <div className='h-screen border-r-[2px] border-gray-100 w-20 bg-white block'>
+                <div className='rounded-full text-center text-white py-2 m-4 bg-blue-600 font-semibold text-xl'>D</div>
+                <div className="bg-[#f0f3f8] w-[55px] rounded-md mx-auto flex flex-col items-center">
+                    {menuItem.map((item, index) => (
+                        <NavLink
+                            className='text-xl'
+                            to={item.path}
+                            key={index}
+                            isActive={() => {
+                                // Check if the current location matches the path
+                                   return location.pathname === item.path;
+                            }}
+                        >
+                            <div className={`px-4 text-center mx-auto py-3 font-semibold ${location.pathname === item.path ? 'text-white' : 'hover:text-blue-600'} ${location.pathname === item.path ? 'bg-blue-600 rounded-md' : 'hover:bg-gray-200'}`}>
+                                {item.icon}
+                            </div>
+                        </NavLink>
+                    ))}
+                </div>
+            </div>
         </div>
-        <main className='w-full bg-sky-300'>{children}</main>
-    </div>
-  )
+    );
 }
