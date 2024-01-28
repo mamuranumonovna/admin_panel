083f5a75f3dcd4236b8a41406744e3d52ec75a1c
Axmatova <axmatovamamura0203@gmail.com>
2024-01-29 00:17:29 +0500

    sidebar changed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,8 @@ import { HiUsers } from 'react-icons/hi2';
 import { IoMdSettings } from 'react-icons/io';
 import { MdAnalytics, MdMyLocation } from "react-icons/md";
 import { IoMdCart } from "react-icons/io";
+import { IoMdNotifications } from "react-icons/io";
+import client from '../assets/clientGirl.svg'
 
 export default function Sidebar({ children }) {
     const location = useLocation(); 
@@ -24,17 +26,14 @@ export default function Sidebar({ children }) {
         {
             path: '',
             icon: <MdMyLocation />
-        },
-        {
-            path: '',
-            icon: <IoMdSettings />
-        },
+        }
        
     ];
 
     return (
-        <div className='flex h-screen'>
-            <div className='h-screen border-r-[2px] border-gray-100 w-20 bg-white block'>
+        <div className='flex flex-col items-center justify-between bg-white'>
+            <div className='flex h-[86vh]'>
+            <div className='h-screen border-r-[2px] border-gray-100 w-20  block'>
                 <div className='rounded-full text-center text-white py-2 m-4 bg-blue-600 font-semibold text-xl'>D</div>
                 <div className="bg-[#f0f3f8] w-[55px] rounded-md mx-auto flex flex-col items-center">
                     {menuItem.map((item, index) => (
@@ -43,17 +42,23 @@ export default function Sidebar({ children }) {
                             to={item.path}
                             key={index}
                             isActive={() => {
-                                // Check if the current location matches the path
+                                
                                    return location.pathname === item.path;
                             }}
                         >
-                            <div className={`px-4 text-center mx-auto py-3 font-semibold ${location.pathname === item.path ? 'text-white' : 'hover:text-blue-600'} ${location.pathname === item.path ? 'bg-blue-600 rounded-md' : 'hover:bg-gray-200'}`}>
+                            <div className={`px-4 text-center mx-auto py-3 font-semibold text-gray-700 ${location.pathname === item.path ? 'text-white' : 'hover:text-blue-600'} ${location.pathname === item.path ? 'bg-blue-600 rounded-md' : 'hover:bg-gray-200'}`}>
                                 {item.icon}
                             </div>
                         </NavLink>
                     ))}
                 </div>
             </div>
+        </div>
+        <div className='flex flex-col items-center gap-6 text-xl text-gray-700'>
+        <IoMdNotifications />
+        <IoMdSettings />
+        <img src={client}/>
+        </div>
         </div>
     );
 }
